3e5a32b7f3bb2b1da70a5554acfc5748e7b81a3b
Crevan <a.siverskii@gmail.com>
2022-12-05 18:18:52 +0100

    [add] Message

---
+++ src/main/java/com/crevan/jchat/Message.java
@@ -0,0 +1,25 @@
+package com.crevan.jchat;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    private final MessageType messageType;
+    private final String data;
+
+    public Message(MessageType messageType) {
+        this(messageType, null);
+    }
+
+    public Message(MessageType messageType, String data) {
+        this.messageType = messageType;
+        this.data = data;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
