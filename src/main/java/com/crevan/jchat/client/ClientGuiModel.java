f9013bb02fbadd54b7ebd30f06171b91814d01c2
Crevan <a.siverskii@gmail.com>
2022-12-05 18:21:07 +0100

    [add] add Gui Client model

---
+++ src/main/java/com/crevan/jchat/client/ClientGuiModel.java
@@ -0,0 +1,30 @@
+package com.crevan.jchat.client;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ClientGuiModel {
+    private final Set<String> allUserNames = new HashSet<>();
+    private String message;
+
+    public Set<String> getAllUserNames() {
+        return Collections.unmodifiableSet(allUserNames);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void addUser(String newUserName) {
+        allUserNames.add(newUserName);
+    }
+
+    public void deleteUser(String newUserName) {
+        allUserNames.remove(newUserName);
+    }
+}
